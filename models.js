4062e19b2c03f9011a49182bbd19f26ae2443d49
Andris Sīlis <Andris.Silis@gmail.com>
2014-04-26 19:28:47 +0300

    Temperature model

--- models.js
+++ models.js
@@ -6,9 +6,12 @@ var BaseSchema = new mongoose.Schema({
 });
 BaseSchema.index({ ts: 1 });
 
+
 var HeartbeatSchema = BaseSchema;
+var TemperatureSchema = BaseSchema;
 
 
 module.exports = {
-	Heartbeat: mongoose.model('Heartbeat', HeartbeatSchema)
+	Heartbeat: mongoose.model('Heartbeat', HeartbeatSchema),
+	Temperature: mongoose.model('Temperature', TemperatureSchema)
 };
