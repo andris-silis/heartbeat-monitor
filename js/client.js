9bc23f9d83fd461f3a1866d5e99bc52f0e9f5139
Andris Sīlis <Andris.Silis@gmail.com>
2014-04-26 11:13:06 +0300

    Smoothie graph

--- js/client.js
+++ js/client.js
@@ -1,16 +1,28 @@
 
-(function (io) {
+(function (io, SmoothieChart, TimeSeries) {
 
 	'use strict';
 	var conn = io.connect();
 
 	// conn.emit('ready');
 
+	var smoothie = new SmoothieChart();
+	smoothie.streamTo(document.getElementById('mycanvas'));
+
+	var line1 = new TimeSeries();
+	line1.append(new Date().getTime(), Math.random());
+
+	smoothie.addTimeSeries(
+		line1,
+		{ strokeStyle: 'rgb(0, 255, 0)', lineWidth: 2 }
+	);
+
 	conn.on('heartbeat', function (data) {
-		console.log(data);
+		// console.log(data);
+		line1.append(new Date().getTime(), data);
 	});
 
 	window.conn = conn;
 
-})(window.io);
+})(window.io, window.SmoothieChart, window.TimeSeries);
 
